00db7b0423d09443974393db66e9e80df6277354
sghaha <sghaha@naver.com>
2022-09-12 03:46:41 +0900

    옵저버패턴 step3 : 체감온도 추가해보기

--- src/observer/Main.java
+++ src/observer/Main.java
@@ -1,6 +1,7 @@
 package observer;
 
 import observer.display.CurrentConditionDisplay;
+import observer.display.HeatIndexDisplay;
 
 public class Main {
 
@@ -8,10 +9,14 @@ public class Main {
 		WeatherData weatherData = new WeatherData();
 
 		CurrentConditionDisplay currentDisplay = new CurrentConditionDisplay(weatherData);
+		HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData);
 
 		weatherData.setMeasurements(80, 65, 30.4f);
+		System.out.println("====================================");
 		weatherData.setMeasurements(70, 55, 20.4f);
+		System.out.println("====================================");
 		weatherData.setMeasurements(60, 45, 10.4f);
+		System.out.println("====================================");
 		weatherData.setMeasurements(50, 35, 5.4f);
 
 	}
